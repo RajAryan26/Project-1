54505883c021cb40b6d6d2b532a3efdf48bcb05a
rajaryan26 <raryan121-comp@bvucoep.edu.in>
2023-02-21 23:31:18 +0530

    Initial Commit

---
+++ pttl.py
@@ -0,0 +1,15 @@
+n= int(input("Enter your 10th class marks: "))
+if(n<0 and n>100):
+    s=int(input("enter your 12th class marks: "))
+         if s>60 and s<=100:
+            a=int(input("Enter your age: "))
+             if a>18:
+                print("Eligible for placement")
+            else:
+                print("Not eligible")
+        else:
+             print("not eligible")
+            
+    
+    else:
+        print("not eligible")
---
+++ pttl2.py
@@ -0,0 +1,9 @@
+def Nsum(n):
+    if n <= 1:
+        return n
+    else:
+        return n + Nsum(n-1)
+n = int(input("Enter a number: "))
+# add=Nsum
+print("The sum is",Nsum(n));
+print("hello world")
